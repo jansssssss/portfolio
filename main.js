c819c98b0cc6cba1e41d5f7d6a865b32bef9ce13
jansssssss <achmadfauzansyaefudin@gmail.com>
2025-10-01 01:15:12 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Ex Small Bussines Owner", "Html Advance",  "Web Developer", "Python Programer Beginner"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
